59ced40c74d1340f191f7fc1ba2097eac40c35c6
Alice Cecil <alycecil@gmail.com>
2018-09-12 15:12:14 -0400

    Initial"

---
+++ Source/Constants.cs
@@ -0,0 +1,64 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Gastroliths
+{
+    class Constants
+    {
+
+        public static bool is_animal(Pawn pawn)
+        {
+            return pawn.RaceProps.Animal;
+        }
+
+        public static Thing makeGastrolith()
+        {
+            ThingDef what = Constants.whatGastrolith();
+            if (what != null)
+            {
+                Log.Message("Generating Gastrolith", false);
+                Thing t = ThingMaker.MakeThing(what, null);
+                t.stackCount = Rand.RangeInclusive(0, 6) + 1;
+                return t;
+            }
+            else
+            {
+                Log.Message("Gastrlith declined", false);
+            }
+
+            return null;
+        }
+
+        public static ThingDef whatGastrolith()
+        {
+            float roll = Rand.Value;
+            //	Roll 1d20: 1-10 Nothing, 10-15 Chunks, 16 - Component, 17- Plastisteel, 18- Silver, 19-Gold, 20-Steel
+            if (roll < .95f)
+            {
+                return ThingDefOf.Steel;
+            }else if (roll < .90f)
+            {
+                return ThingDefOf.Gold;
+            }else if (roll < .85f)
+            {
+                return ThingDefOf.Silver;
+            }
+            else if (roll < .80f)
+            {
+                return ThingDefOf.Plasteel;
+            }
+            else if (roll < .75f)
+            {
+                return ThingDefOf.ComponentIndustrial;
+            }
+            else if (roll < .50f)
+            {
+                return ThingDefOf.Granite;
+            }
+            else
+            {
+                return ThingDefOf.Hay;
+            }
+        }
+    }
+}
---
+++ Source/Harmony.cs
@@ -0,0 +1,41 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Gastroliths
+{
+    class Harmony
+    {
+        [HarmonyPatch(typeof(GenRecipe), "MakeRecipeProducts", null)]
+        public static class MakeRecipeProductsnPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients)
+            {
+
+                Log.Message("0HarmonyHook, butcher", false);
+                List<Thing> list = (__result as List<Thing>) ?? __result.ToList<Thing>();
+                
+                foreach (Corpse corpse in ingredients.OfType<Corpse>())
+                {
+                    Log.Message("0HarmonyHook, butchering corpse; adding gastrolith", false);
+                    Thing var = Constants.makeGastrolith();
+
+                    if (var != null)
+                    {
+                        list.Add(var);
+                    }
+                }
+
+                __result = list;
+                return;
+            }
+
+
+        }
+    }
+}
+
